41c7cd47e2bb2598428337e234730504c01f4fde
Carlos Alves <carlosalves369m@hotmail.com>
2022-12-24 11:10:24 -0300

    :sparkles: create HTTPs calls with cache check

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import NodeCache from "node-cache";
+import express, { Request, response, Response } from "express";
+import config from './express.config'
+import axios from "axios";
+
+const cache = new NodeCache({ stdTTL: 10 });
+const app = express();
+
+app
+    .get('/', async (req: Request, res: Response) => {
+        if(cache.has(config.CACHE_NAME)) {
+            console.log('Using Cache');
+            return res.send(cache.get(config.CACHE_NAME))
+        } else {
+            const response = await axios.get(config.URL);
+
+            cache.set(config.CACHE_NAME, response.data);
+            console.log('Getting data from the API');
+
+            res.json(response.data)
+        }
+    })
+
+    .get('/stats', (req: Request, res: Response) => {
+        res.send(cache.getStats())
+    })
+
+    .listen(config.PORT, () => console.log(config.MESSAGE))
